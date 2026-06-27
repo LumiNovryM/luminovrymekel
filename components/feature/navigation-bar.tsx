0c1e709d6bd2500bc49d5326d41e7e243ebc39dc
LumiNovryM <luminovrymekel@gmail.com>
2026-06-27 15:44:31 +0700

    dev: update navbar ui

--- components/feature/navigation-bar.tsx
+++ components/feature/navigation-bar.tsx
@@ -9,51 +9,41 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
 export function NavigationBar() {
   return (
-    <NavigationMenu className="bg-none absolute top-0 right-0 z-50">
+    <NavigationMenu
+      className="rounded-none absolute top-0 right-0 z-50 text-white my-4 px-11 pb-5 bg-transparent border-b-2 border-white/10 shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[2.2px]"
+    >
       {/* Menu List */}
-      <NavigationMenuList>
-        {/* Home */}
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            render={<Link href="#">Home</Link>}
+      <NavigationMenuList className="flex items-center justify-between gap-4">
+        {/* Uchiha Clan & Katakana Name Icon */}
+        <NavigationMenuItem className="flex items-center justify-center">
+          <Image
+            src="/assets/Uchiha-Clan.png"
+            alt="Next.js logo"
+            width={45}
+            height={45}
+            className="bg-[#05050d]"
+            priority
+          />
+          <Image
+            src="/assets/Katakana-Name.png"
+            alt="Next.js logo"
+            width={100}
+            height={45}
+            className="bg-[#05050d]"
+            priority
           />
         </NavigationMenuItem>
-        {/* About */}
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            render={<Link href="#">About</Link>}
-          />
-        </NavigationMenuItem>
-        {/* Skills */}
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            render={<Link href="#">Skills</Link>}
-          />
-        </NavigationMenuItem>
-        {/* Projects */}
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            render={<Link href="#">Projects</Link>}
-          />
-        </NavigationMenuItem>
-        {/* Contact */}
-        <NavigationMenuItem>
-          <NavigationMenuLink
-            className={navigationMenuTriggerStyle()}
-            render={<Link href="#">Contact</Link>}
-          />
-        </NavigationMenuItem>
-        {/* Sharinggan Icon */}
-        <NavigationMenuItem>
+        <NavigationMenuItem className="flex items-center justify-center gap-11">
+          <NavigationMenuLink render={<Link href="#">Home</Link>} />
+          <NavigationMenuLink render={<Link href="#">About</Link>} />
+          <NavigationMenuLink render={<Link href="#">Skills</Link>} />
+          <NavigationMenuLink render={<Link href="#">Projects</Link>} />
+          <NavigationMenuLink render={<Link href="#">Contact</Link>} />
+          {/* Sharinggan Icon */}
           <Image
             src="/assets/Sharinggan-Image.png"
             alt="Next.js logo"
--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -34,7 +34,7 @@ function NavigationMenuList({
     <NavigationMenuPrimitive.List
       data-slot="navigation-menu-list"
       className={cn(
-        "group flex flex-1 list-none items-center justify-center gap-0",
+        "group flex flex-1 list-none items-center justify-center gap-4",
         className
       )}
       {...props}
@@ -129,7 +129,7 @@ function NavigationMenuLink({
     <NavigationMenuPrimitive.Link
       data-slot="navigation-menu-link"
       className={cn(
-        "flex items-center gap-2 rounded-lg p-2 text-sm transition-all outline-none hover:bg-muted focus:bg-muted focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:outline-1 in-data-[slot=navigation-menu-content]:rounded-md data-active:bg-muted/50 data-active:hover:bg-muted data-active:focus:bg-muted [&_svg:not([class*='size-'])]:size-4",
+        "flex items-center gap-2  p-2 text-sm transition-all outline-none hover:bg-none hover:border-b-2 hover:border-b-purple-600 hover:text-purple-600 focus:text-purple-600 focus:border-b-2 focus:border-b-purple-600 focus-visible:ring-3 focus-visible:ring-ring/50 focus-visible:outline-1 in-data-[slot=navigation-menu-content]:rounded-md data-active:bg-muted/50 data-active:hover:bg-muted data-active:focus:bg-muted [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
